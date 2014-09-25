85ce1da99a3ef61f307b156c0a62b70346eb7db1
Anton Ulyanov <anton.ulianov@gmail.com>
2014-09-25 19:04:08 +0400

    Header introduced

--- admin/routes/index.js
+++ admin/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.use('/partials', function(req, res) {
+  res.render('partials' + req.url);
+});
+
 module.exports = router;
