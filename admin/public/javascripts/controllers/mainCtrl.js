9808ce1e22a8aa1e060128fc13093188946fbf97
Anton Ulyanov <anton.ulianov@gmail.com>
2014-09-26 18:27:57 +0400

    servers list markup/files

--- admin/public/javascripts/controllers/mainCtrl.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-app.controller('MainCtrl', function ($scope){
-  $scope.x=1;
-});
---
+++ admin/public/javascripts/controllers/serversCtrl.js
@@ -0,0 +1,8 @@
+'use strict';
+app.controller('ServersCtrl', function ($scope){
+  $scope.servers = [{
+    name:'Test'
+  }, {
+    name:'Test2'
+  }]
+});
