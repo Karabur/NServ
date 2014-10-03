1340d716fc3afda18a604c1c23f8191ceeb7756f
Anton Ulyanov <anton.ulianov@gmail.com>
2014-10-04 01:53:00 +0400

    Load servers from config

--- admin/routes/api.js
+++ admin/routes/api.js
@@ -4,11 +4,10 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/servers', function(req, res) {
-  res.json([{
-    name:'Test'
-  }, {
-    name:'Test2'
-  }]);
+
+  var config = require("../../config.json");
+
+  res.json(config['servers']);
 });
 
 
